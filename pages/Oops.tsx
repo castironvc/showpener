fc52fd83d4542493c7272ddc0ea9b6d9c42f71ed
Mike Gamaroff <mike@gamaroff.net>
2022-05-30 08:44:28 -0700

    Thanks styles

--- pages/Oops.tsx
+++ pages/Oops.tsx
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import styles from "../styles/connectSpotify.module.css";
+import styles from "../styles/thanks.module.css";
 import { AppContext } from "../context/StateContext";
 
 function Oops() {
@@ -19,45 +19,27 @@ function Oops() {
       pathname: `/`,
     });
   };
-
+  useEffect(() => {
+    if (status && status === "unauthenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
   console.log(state);
   return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <div>
-          <div className={styles.card}>
-            <h1 className={styles.mainTitle}>Oops!</h1>
-            <div>
-              <Image
-                src="/images/Showpener_logo_transparent.svg"
-                width={140}
-                height={60}
-                className=""
-                alt=""
-              />
-            </div>
-            <div className={styles.textContainer}>
-              <span> Looks like we ran into some trouble.</span>
-            </div>
-
-            <div className={styles.notice}>{router.query.message}</div>
-
-            <div className={styles.buttonContainer}>
-              <button
-                className={styles.submitButton}
-                onClick={status && status === "authenticated" ? logOut : goHome}
-              >
-                <span>Try again</span>
-              </button>
-            </div>
-          </div>
-
-          <div className={styles.disclaimer}>
-            We will not be sharing your personal information with{" "}
-            <span className="widow">third parties.</span>
-          </div>
-        </div>
+    <div className={styles.centerColumnContent}>
+      <h1>Oops!</h1>
+      <div className={styles.messageContainer}>
+        <p>Looks like we ran into some trouble.</p>
       </div>
+
+      <div className="notice">{router.query.message}</div>
+
+      <button
+        className="submitButton"
+        onClick={status && status === "authenticated" ? logOut : goHome}
+      >
+        <span>Try again</span>
+      </button>
     </div>
   );
 }
